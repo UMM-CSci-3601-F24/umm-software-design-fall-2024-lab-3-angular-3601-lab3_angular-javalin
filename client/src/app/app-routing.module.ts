1ce701f28cc23ba95af4c3d19b4a36c43eb10f51
JosieBarber <barbe497@umn.edu>
2024-09-20 03:47:30 -0500

    Get rid of some developing testing things

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './users/user-list.component';
 import { UserProfileComponent } from './users/user-profile.component';
 import { TodoListComponent } from './todov2/todo-list/todo-list.component';
-import { TodoCardComponent } from './todov2/todo-card/todo-card.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
   { path: 'users', component: UserListComponent, title: 'Users' },
   { path: 'users/:id', component: UserProfileComponent, title: 'User Profile' },
   { path: 'todos', component: TodoListComponent, title: 'Todos' },
-  { path: 'card', component: TodoCardComponent, title: 'testingCard' },
-
-
 
 ];
 
